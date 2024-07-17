1205855a09f5dac9ec72d44b262716e455b20b47
zhuweiming <918198027@qq.com>
2024-07-17 22:49:50 +0800

    前后端联调 | 用户自动登录：前端(access\index.ts--业务逻辑)调用后端登录接口

--- oj-frontend/src/App.vue
+++ oj-frontend/src/App.vue
@@ -11,9 +11,7 @@
 
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
-import { useRouter } from "vue-router";
-import { useStore } from "vuex";
-import ASSESS_ENUM from "@/access/accessEnum";
+
 import { onMounted } from "vue";
 
 /**
@@ -25,20 +23,5 @@ const doInit = () => {
 
 onMounted(() => {
   doInit();
-});
-
-const router = useRouter();
-const store = useStore();
-
-router.beforeEach((to, from, next) => {
-  //判断to的页面meta是否有需要管理员用户的标识
-  if (to.meta?.access === ASSESS_ENUM.ADMIN) {
-    //是，查询已登录用户是否有权限
-    if (store.state.user.loginUser?.userRole !== ASSESS_ENUM.ADMIN) {
-      next("/noAuth");
-      return;
-    }
-  }
-  next();
 });
 </script>
--- oj-frontend/src/store/user.ts
+++ oj-frontend/src/store/user.ts
@@ -1,6 +1,8 @@
 // initial state
 import { StoreOptions } from "vuex";
 import ASSESS_ENUM from "@/access/accessEnum";
+import { UserControllerService } from "../../generated";
+import AccessEnum from "@/access/accessEnum";
 
 export default {
   namespaced: true,
@@ -8,13 +10,21 @@ export default {
   state: () => ({
     loginUser: {
       userName: "未登录",
-      userRole: ASSESS_ENUM.NOT_LOGIN,
     },
   }),
 
   actions: {
-    getLoginUser({ commit, state }, payload) {
-      //TODO 改成远程登录
+    //从远程请求获取登录信息
+    async getLoginUser({ commit, state }, payload) {
+      const res = await UserControllerService.getLoginUserUsingGet();
+      if (res.code === 0) {
+        commit("updateUser", res.data);
+      } else {
+        commit("updateUser", {
+          ...state.loginUser,
+          userRole: AccessEnum.NOT_LOGIN,
+        });
+      }
       commit("updateUser", payload);
     },
   },
